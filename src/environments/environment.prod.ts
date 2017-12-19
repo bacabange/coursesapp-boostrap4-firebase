c1cd66197856de6884b252bdc22eed781469ac79
Stiven Castillo <stiven.castillo@creardigital.com>
2017-12-19 13:52:29 -0500

    Remove some config files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCbCXZGFF4SGW-V5WpEdh0GTIUMk8X0vok',
-    authDomain: 'finlog-1495553525685.firebaseapp.com',
-    databaseURL: 'https://finlog-1495553525685.firebaseio.com',
-    projectId: 'finlog-1495553525685',
-    storageBucket: 'finlog-1495553525685.appspot.com',
-    messagingSenderId: '191729935656'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
